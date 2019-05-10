9b86392d9930b7e8b3005747cbaacb1642c3c3e0
Kenny Huynh <huynhkenny125@yahoo.com>
2019-05-10 16:01:41 -0700

    added eslint to client, added travis.yml (#1)
    * added eslint to client, added travis.yml
    
    * fix typo for travis file
    
    * fix travis before_install
    
    * lint fix / postgres service
    
    * add installation for client
    
    * try psql command again
    
    * removed command on before_install
    
    * add creation of db in before_install
    
    * cd back to main directiory
    
    * added script to create table
    
    * add extension, rename
    
    * fix sql extension command
    
    * syntax fix
    
    * create users before posts
    
    * change order

---
+++ server/scripts/setup-test-db.js
@@ -0,0 +1,43 @@
+const Promise = require('bluebird');
+const db = require('../db');
+
+async function run() {
+  try {
+    await db.query('BEGIN');
+    await db.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await db.query('DROP TABLE IF EXISTS posts');
+    await db.query('DROP TABLE IF EXISTS users');
+    await db.query(`CREATE TABLE "public"."users" (
+      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+      "first_name" text,
+      "last_name" text,
+      "password" varchar NOT NULL,
+      "created_at" timestamptz NOT NULL DEFAULT now(),
+      "updated_at" timestamptz NOT NULL DEFAULT now(),
+      "email" text NOT NULL,
+      PRIMARY KEY ("id")
+    );`);
+    await db.query(`CREATE TABLE "public"."posts" (
+      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+      "title" text NOT NULL,
+      "content" varchar NOT NULL,
+      "user_id" uuid NOT NULL,
+      "created_at" timestamptz NOT NULL DEFAULT now(),
+      "updated_at" timestamptz NOT NULL DEFAULT now(),
+      "deleted_at" timestamptz,
+      CONSTRAINT "posts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+      PRIMARY KEY ("id")
+    );`);
+    await db.query('COMMIT');
+  } catch (e) {
+    await db.query('ROLLBACK');
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+  }
+}
+
+Promise.resolve()
+  .then(run)
+  // eslint-disable-next-line no-console
+  .catch(err => console.log(`ERROR: ${err.stack || err}`))
+  .finally(process.exit);
