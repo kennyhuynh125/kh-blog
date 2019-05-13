ef69d0e2d97ee9baf1d073261ff9b16d77d04851
Kenny Huynh <huynhkenny125@yahoo.com>
2019-05-13 00:15:04 -0700

    Feature/add user (#4)
    * add test, functions, and routes for users
    
    * lint fix

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,51 @@
-// const bcrypt = require('bcrypt');
-// const db = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../db');
 
-// const checkPassword = password => null;
+const SALT_ROUNDS = 10;
 
-// const logIn = async ({ email, password }) => null;
+const checkPassword = async (password, hashedPassword) => bcrypt.compare(password, hashedPassword);
 
-// const createUser = async ({ email, password }) => null;
+const logIn = async ({ email, password }) => {
+  try {
+    await db.query('BEGIN');
+    const query = 'SELECT * FROM users WHERE email = ($1)';
+    const values = [email];
+    const { rows } = await db.query(query, values);
+    if (rows.length === 0) {
+      throw new Error(`No user found with email ${email}`);
+    }
+    const [user] = rows;
+    const hasCorrectPassword = await checkPassword(password, user.password);
+    if (!hasCorrectPassword) {
+      throw new Error('Incorrect password');
+    }
+    const token = await jwt.sign({ email }, process.env.SECRET_TOKEN_STRING, {
+      expiresIn: '1h',
+    });
+    return token;
+  } catch (e) {
+    await db.query('ROLLBACK');
+    throw new Error(e);
+  }
+};
 
-// module.exports = {
-//   createUser,
-//   logIn,
-// };
+const createUser = async ({ email, password }) => {
+  try {
+    const hashPass = await bcrypt.hash(password, SALT_ROUNDS);
+    await db.query('BEGIN');
+    const query = 'INSERT INTO users (email, password) VALUES ($1, $2)';
+    const values = [email, hashPass];
+    const { rows } = await db.query(query, values);
+    await db.query('COMMIT');
+    return rows;
+  } catch (e) {
+    await db.query('ROLLBACK');
+    throw new Error(e);
+  }
+};
+
+module.exports = {
+  createUser,
+  logIn,
+};
---
+++ server/test/controllers/users.spec.js
@@ -0,0 +1,66 @@
+const bcrypt = require('bcrypt');
+const { expect } = require('chai');
+const db = require('../../db');
+const usersController = require('../../controllers/users');
+
+describe('user Controller', () => {
+  before(async () => {
+    await db.query('DELETE FROM users');
+  });
+
+  context('createUser', () => {
+    it('should create a user', async () => {
+      const response = await usersController.createUser({
+        email: 'test@test.com',
+        password: 'test',
+      });
+      expect(response).to.be.an('array').with.length(0);
+      const { rows } = await db.query('SELECT * FROM users');
+      expect(rows).to.be.an('array').with.length(1);
+      const [user] = rows;
+      expect(user.email).to.equal('test@test.com');
+      // check that password is hashed
+      expect(user.password).not.to.equal('test');
+      const compare = await bcrypt.compare('test', user.password);
+      expect(compare).to.equal(true);
+    });
+  });
+
+  context('logIn', () => {
+    before(async () => {
+      await usersController.createUser({
+        email: 'test@test.com',
+        password: 'test',
+      });
+    });
+    it('should log a user in by creating a token', async () => {
+      const response = await usersController.logIn({
+        email: 'test@test.com',
+        password: 'test',
+      });
+      expect(response).to.not.equal(null);
+    });
+    it('should throw an error if no email found', async () => {
+      try {
+        await usersController.logIn({
+          email: 'random@random.com',
+          password: 'test',
+        });
+        throw new Error('Should have thrown!');
+      } catch (e) {
+        expect(e.message).to.equal('Error: No user found with email random@random.com');
+      }
+    });
+    it('should throw an error if password is incorrect', async () => {
+      try {
+        await usersController.logIn({
+          email: 'test@test.com',
+          password: 'testt',
+        });
+        throw new Error('Should have thrown!');
+      } catch (e) {
+        expect(e.message).to.equal('Error: Incorrect password');
+      }
+    });
+  });
+});
