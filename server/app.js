af9e7e1523e6c8c9b11763a98f3dd9b5919dad47
Kenny Huynh <huynhkenny125@yahoo.com>
2019-05-28 10:15:35 -0700

    added express static to app.js, heroku-postbuild, procfile (#8)

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 const bodyParser = require('./middleware/bodyParser');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
 app.use(bodyParser);
 routes(app);
