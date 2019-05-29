0c156a9d85d34fbe7bec59e306a3ece3e03a0b72
Kenny Huynh <kennyhuynh@TSGAdmins-MBP.T-mobile.com>
2019-05-29 10:51:03 -0700

    fix catchall handler issue + added redux logger

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ const bodyParser = require('./middleware/bodyParser');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 app.use(cors());
 app.use(bodyParser);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
-});
 routes(app);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+});
 
 module.exports = app;
