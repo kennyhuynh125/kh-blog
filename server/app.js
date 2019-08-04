90cbcbff25f1b84bf30e2fbc6f51c20880d18db0
Kenny Huynh <kennyhuynh@TSGAdmins-MacBook-Pro.local>
2019-08-04 10:05:12 -0700

    added winston and helmet

--- server/app.js
+++ server/app.js
@@ -1,14 +1,31 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const path = require('path');
+const winston = require('winston');
 const routes = require('./routes');
 const bodyParser = require('./middleware/bodyParser');
+
+const { winstonConfig } = require('./config');
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(winstonConfig.file),
+    new winston.transports.Console(winstonConfig.console),
+  ],
+});
+logger.stream = {
+  write: message => logger.info(message),
+};
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../client/build')));
 app.use(cors());
+app.use(helmet());
+app.use(morgan('combined', { stream: logger.stream }));
 app.use(bodyParser);
 routes(app);
 app.get('*', (req, res) => {
